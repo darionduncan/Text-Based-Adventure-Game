3b83423b2eaf5d2bf782afa1c1182661fcee90b0
darionduncan <230998204+darionduncan@users.noreply.github.com>
2025-09-10 20:47:44 +0100

    Introduction added to game
    Added introduction. Not final.

---
+++ Untitled-Text-Based-Adventure-Game.py
@@ -0,0 +1,10 @@
+import time
+#Introduction to Game Demo
+print("Hello, and welcome to the Untitled Text Based Adventure Game!")
+time.sleep(0.5)
+user = input("Enter your desired username: ")
+time.sleep(1)
+print("Hello, ",user,", welcome to the Untitled Text Based Adventure Game!")
+time.sleep(0.5)
+print("Before we begin, please note that this is only a demo. This is not the full game")
+time.sleep(0.5)
