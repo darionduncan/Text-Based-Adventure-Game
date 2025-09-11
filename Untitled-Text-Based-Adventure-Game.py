863889326a2d10b86d970e85ab8a7c0a7c810604
darionduncan <230998204+darionduncan@users.noreply.github.com>
2025-09-11 12:19:34 +0100

    Corrected an error
    added a space

--- Untitled-Text-Based-Adventure-Game.py
+++ Untitled-Text-Based-Adventure-Game.py
@@ -1,6 +1,6 @@
 import time
 #Introduction to Game Demo
-gamename= "Untitled Text Based Adventure Game"
+gamename = "Untitled Text Based Adventure Game"
 print("Hello, and welcome to the ", gamename,"!")
 time.sleep(0.5)
 user = input("Enter your desired username: ")
@@ -9,3 +9,4 @@ print("Hello, ",user,", welcome to the ", gamename,"!")
 time.sleep(0.5)
 print("Before we begin, please note that this is only a demo. This is not the full game")
 time.sleep(0.5)
+
